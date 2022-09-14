114b37f021d59d3ade07e3e224f35699a5d06235
Philipp E <philipp3923@icloud.com>
2022-09-14 18:57:37 +0200

    user account creation

--- server_auth/db.js
+++ server_auth/db.js
@@ -1,13 +1,32 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.MARIADB_HOST,
+    port: process.env.MARIADB_PORT,
     user: process.env.MARIADB_USER,
     database: process.env.MARIADB_DB,
     password: process.env.MARIADB_PASSWORD,
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected to database");
-  });
+pool.on("error", (err) => {
+    console.log(err);
+});
+
+function queryPromise(query_str, query_var) {
+    return new Promise((resolve, reject) => {
+        try {
+            pool.query(query_str, query_var, function (err, rows) {
+                if (err) {
+                    console.log(err);
+                    return reject(err);
+                }
+                resolve(rows);
+            });
+        } catch (err) {
+            console.log(err);
+            return reject(err);
+        }
+    });
+}
+
+module.exports = { queryPromise };
--- server_auth/index.js
+++ server_auth/index.js
@@ -3,36 +3,74 @@ const jwt = require("jsonwebtoken");
 const express = require("express");
 const app = express();
 const db = require("./db");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
-app.post("/register");
+app.post("/register", onRegister);
 app.post("/login", onLogin);
 app.post("/logout");
 app.post("/refreshToken", onRefreshToken);
 
 app.listen(5000, () => console.log("server_auth listening on port 5000"));
 
-function onLogin(req, res) {
+function onLogin(req, res) {}
+
+async function onRegister(req, res) {
     if (!req.body.email || !req.body.password) {
         return res.sendStatus(406);
     }
 
-    //auth user in db and get public_id !
-    const user = {
-        email: req.body.email,
-        public_id: "RANDOM_STRING",
-    };
+    let query_getAccount = "SELECT * FROM accounts WHERE email = ?;";
 
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
+    await db
+        .queryPromise(query_getAccount, [req.body.email])
+        .then((rows) => {
+            if (rows.length > 0) {
+                return res.sendStatus(409);
+            }
+            console.log(rows);
 
-    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+            const salt = bcrypt.genSaltSync(10);
+            const hashed_password = bcrypt.hashSync(req.body.password, salt);
+
+            const user = {
+                email: req.body.email,
+            };
+
+            const accessToken = generateAccessToken(user);
+            const refreshToken = generateRefreshToken(user);
+
+            let query_insertAccount =
+                "INSERT INTO accounts(email, password, account_creation) VALUES (?,?,NOW());";
+            let query_insertToken =
+                "INSERT INTO tokens(content, token_creation, account_id) VALUES (?, NOW(), (SELECT account_id FROM accounts WHERE email = ?))";
+
+            db.queryPromise(query_insertAccount, [
+                req.body.email,
+                hashed_password,
+            ])
+                .then(() => {
+                    db.queryPromise(query_insertToken, [
+                        refreshToken,
+                        req.body.email,
+                    ]);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+            res.json({ accessToken: accessToken, refreshToken: refreshToken });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 }
 
-function onRefreshToken(req, res){
+function onRefreshToken(req, res) {
     const refreshToken = req.body.token;
-    if(refreshToken == null) return res.sendStatus(401);
+    if (refreshToken == null) return res.sendStatus(401);
 }
 
 function generateAccessToken(user) {
@@ -40,5 +78,5 @@ function generateAccessToken(user) {
 }
 
 function generateRefreshToken(user) {
-    return jwt.sign(user, process.env.JWT_REFRESH_SECRET);
+    return jwt.sign(user, process.env.JWT_REFRESH_SECRET, { expiresIn: "30d" });
 }
