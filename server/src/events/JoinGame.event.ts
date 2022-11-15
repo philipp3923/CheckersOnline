fed8cc833b13825d38fd7945d77fe7dac642174a
Philipp E <philipp3923@icloud.com>
2022-11-15 13:18:36 +0100

    async api doku start

--- server/src/events/JoinGame.event.ts
+++ server/src/events/JoinGame.event.ts
@@ -1,37 +1,43 @@
 import AbstractEvent from "./Abstract.event";
-import SocketService, {SocketResponse} from "../services/Socket.service";
+import SocketService, { SocketResponse } from "../services/Socket.service";
 import GameService from "../services/Game.service";
 import Connection from "../models/Connection.model";
 
 export default class JoinGameEvent extends AbstractEvent {
-    public constructor(socketService: SocketService, private gameService: GameService) {
-        super(socketService, "joinGame");
+  public constructor(
+    socketService: SocketService,
+    private gameService: GameService
+  ) {
+    super(socketService, "joinGame");
+  }
+
+  public async on(connection: Connection, args: any, respond: SocketResponse) {
+    if (connection.getGameCount() > 20) {
+      respond({
+        success: false,
+        error: "Exceeded amount of possible simultaneous games",
+      });
+      return;
     }
 
-    public async on(connection: Connection, args: any, respond: SocketResponse) {
-        if (connection.getGameCount() > 20) {
-            respond({error: "Exceeded amount of possible simultaneous games"});
-            return;
-        }
-
-        if(typeof args.key !== "string"){
-            respond({error: "Invalid argument type"});
-            return;
-        }
-
-        const game = this.gameService.getGame(args.key);
-
-        if (game === null) {
-            respond({error: "Game does not exist"});
-            return;
-        }
-
-        try {
-            await connection.joinGame(game);
-        }catch (e) {
-            respond({error: "Cannot join game"});
-            return;
-        }
-
+    if (typeof args.key !== "string") {
+      respond({ success: false, error: "Invalid argument type" });
+      return;
     }
+
+    const game = this.gameService.getGame(args.key);
+
+    if (game === null) {
+      respond({ success: false, error: "Game does not exist" });
+      return;
+    }
+
+    try {
+      await connection.joinGame(game);
+    } catch (e) {
+      respond({ success: false, error: "Cannot join game" });
+      return;
+    }
+    respond({ success: true });
+  }
 }
