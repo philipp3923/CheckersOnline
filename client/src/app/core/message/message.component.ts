63df96d22b5ac7e472f60e5ef0a1b47f3d4a76ca
philipp3923 <philipp3923@icloud.com>
2022-12-16 16:18:32 +0100

    css translate fix

--- client/src/app/core/message/message.component.ts
+++ client/src/app/core/message/message.component.ts
@@ -15,13 +15,13 @@ import {animate, style, transition, trigger} from "@angular/animations";
           [
             style({transform: "translate(-50%,-150%)", opacity: 0}),
             animate('0.6s ease-out',
-              style({transform: "translateY(-50%, 0)", opacity: 1}))
+              style({transform: "translate(-50%, 0)", opacity: 1}))
           ]
         ),
         transition(
           ':leave',
           [
-            style({transform: "translateY(-50%, 0)", opacity: 1}),
+            style({transform: "translate(-50%, 0)", opacity: 1}),
             animate('0.6s ease-in',
               style({transform: "translate(-50%,-150%)", opacity: 0}))
           ]
